2cf0b4afe48ee2d1718570cce7951d5bd83907bf
Samuel Eley <78078726+saeleyyy@users.noreply.github.com>
2024-12-02 17:59:53 +0000

    Day 01 / 02 (#1)
    * added new project
    
    * added sln
    
    * added gitignore file
    
    * ignore all input files
    
    * 02 part solution
    
    * solved part 2
    
    * add 01 work
    
    * changed folder structure
    
    * Delete .DS_Store
    
    ---------
    
    Co-authored-by: Samuel Eley <samueleley@samuels-air.home>
    Co-authored-by: Samuel Eley <samueleley@unknown.home>

---
+++ 2024/01/Program.cs
@@ -0,0 +1,75 @@
+﻿internal class Program
+{
+    private static readonly List<int> locationIdsLeft = new();
+    private static readonly List<int> locationIdsRight = new();
+
+    private static void Main(string[] args)
+    {
+
+        // Part 1
+        // // Step 1: Parse file into two lists (is there a better way of doing this?)
+        // foreach(var line in File.ReadLines("input.txt"))
+        // {
+        //     var split = line.Split("   ");
+
+        //     locationIdsLeft.Add(int.Parse(split[0]));
+        //     locationIdsRight.Add(int.Parse(split[1]));
+        // }
+
+        // // Step 2: Sort the list by ascending order
+        // locationIdsLeft.Sort();
+        // locationIdsRight.Sort();
+
+        // List<int> distances = new();
+        // // Setp 3: Find the distnace between numbers with the same index in each list
+        // foreach (var (left, right) in locationIdsLeft.Zip(locationIdsRight))
+        // {
+        //     distances.Add(Math.Abs(left - right));
+        // }
+
+        // // Step 4: Add all of them together
+        // int result = 0;
+        // foreach (var distance in distances)
+        // {
+        //     result += distance;
+        // }
+
+        // // Correct Result - 2742123
+        // Console.WriteLine(result);
+
+        // Part 2:
+
+        foreach(var line in File.ReadLines("input.txt"))
+        {
+            var split = line.Split("   ");
+
+            locationIdsLeft.Add(int.Parse(split[0]));
+            locationIdsRight.Add(int.Parse(split[1]));
+        }
+
+        // Step 2: Get similarity scores
+        List<int> scores = new();
+        foreach(var leftItem in locationIdsLeft)
+        {
+            int appearances = 0;
+            // How many times does it appear in the right
+            foreach(var rightItem in locationIdsRight)
+            {
+                if (leftItem == rightItem)
+                {
+                    appearances++;
+                }
+            }
+            scores.Add(leftItem * appearances);
+        }
+
+        // Step 3: Get total simlarity score
+        int final = 0;
+        foreach (var score in scores)
+        {
+            final += score;
+        }
+
+        Console.WriteLine(final);
+    }
+}
---
+++ 2024/02/Program.cs
@@ -0,0 +1,104 @@
+﻿internal class Program
+{
+    private static int totalSafe { get; set; } = 0;
+    private static int totalUnsafe { get; set; } = 0;
+    private static int linesProcessed { get; set; } = 0;
+    private static List<int[]> unsafeReports = new();
+    
+    private static void Main(string[] args)
+    {
+        // Part One
+        IdentifySafeReports();
+        // Part Two
+        DampenUnsafeReports();
+    }
+    
+    private static void IdentifySafeReports()
+    {
+        foreach(var report in File.ReadLines("input.txt"))
+        {
+            linesProcessed++;
+
+            // Parse levels into int array
+            var levels = report.Split(" ").Select(int.Parse).ToArray();
+
+            // Determine expected trajectory of report levels to be determined safe
+            LevelTrajectory expectedTrajectory = levels[0] > levels[1] ? LevelTrajectory.Descending : LevelTrajectory.Ascending;
+
+            bool safe = true;
+            foreach (var (curr, next) in levels.Zip(levels.Skip(1)))
+            {
+                var trajectory = curr > next ? LevelTrajectory.Descending : LevelTrajectory.Ascending;
+
+                /*
+                    Mark report as unsafe if current and next level...
+                        are the same value
+                        have a difference of outside the range 1 -> 3
+                        change trajectory i.e. 1, 2, 3, 5. Changes to Ascending at 5
+                        
+                */
+                if (curr == next || Math.Abs(curr - next) > 3 || trajectory != expectedTrajectory)
+                {
+                    totalUnsafe++;
+                    unsafeReports.Add(levels);
+                    safe = false;
+                    break;
+                }
+            }
+
+            if (safe)
+            {
+                totalSafe++;
+            }
+        }
+
+        Console.WriteLine($"\nTotal Safe Reports: {totalSafe}");
+        Console.WriteLine($"Total UnSafe Reports: {totalUnsafe}");
+        Console.WriteLine($"Lines Processed: {linesProcessed}");
+    }
+
+    private static void DampenUnsafeReports()
+    {
+        int newlySafeReports = 0;
+        int counter = 0;
+        foreach (var report in unsafeReports)
+        {
+            counter++;
+            for (int i = 0; i < report.Count(); i++)
+            {
+                var levels = report.ToList();
+                levels.RemoveAt(i);
+
+                LevelTrajectory expectedTrajectory = levels[0] > levels[1] ? LevelTrajectory.Descending : LevelTrajectory.Ascending;
+
+                bool safe = true;
+                foreach (var (curr, next) in levels.Zip(levels.Skip(1)))
+                {
+                    var trajectory = curr > next ? LevelTrajectory.Descending : LevelTrajectory.Ascending;
+
+                    if (curr == next || Math.Abs(curr - next) > 3 || trajectory != expectedTrajectory)
+                    {
+                        safe = false;
+                        break;
+                    };
+                }
+
+                if (safe)
+                {
+                    newlySafeReports++;
+                    break;
+                }
+            }
+        }
+
+        Console.WriteLine($"\nThe Dampening process has produced {newlySafeReports} safe reports");
+        Console.WriteLine($"There are now {totalSafe + newlySafeReports} Safe Reports");
+        Console.WriteLine($"Lines Processed {counter}");
+    }
+}
+
+public enum LevelTrajectory
+{
+    Ascending,
+    Descending
+}
