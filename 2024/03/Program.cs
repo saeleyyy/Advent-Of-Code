9d4ad827351f422bfbb355c57e57eeffccfb64a5
Samuel Eley <samueleley@samuels-air.home>
2024-12-03 15:53:52 +0000

    Part 2 Solved

--- 2024/03/Program.cs
+++ 2024/03/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System.Text.RegularExpressions;
 
-var total  = Regex.Matches(File.ReadAllText("input.txt"), @"mul\(\d{1,3},\d{1,3}\)")
+string corruptedMemory = File.ReadAllText("input.txt");
+
+var total  = Regex.Matches(corruptedMemory, @"mul\(\d{1,3},\d{1,3}\)")
                 .Select(m => m.Value)
                 .Select(m => 
                     m.Replace("mul(", "")
@@ -9,4 +11,35 @@ var total  = Regex.Matches(File.ReadAllText("input.txt"), @"mul\(\d{1,3},\d{1,3}
                         .Select(x => int.Parse(x))
                         .Aggregate((a, x) => a * x)).Sum();
 
-Console.WriteLine($"Result: {total}");
+Console.WriteLine($"Part 1 Result: {total}");
+var x = Regex.Matches(corruptedMemory, @"(mul\(\d{1,3},\d{1,3}\))|(don't\(\))|(do\(\))");
+
+bool calc = true;
+List<string> y = new();
+foreach(var t  in x.Select(x => x.Value))
+{
+    if(t == "don't()")
+    {
+        calc = false;
+        continue;
+    }
+
+    if(t == "do()") {
+        calc = true;
+        continue;
+    };
+
+    if(calc == true)
+    {
+        y.Add(t);
+    }
+}
+
+var res = y.Select(m => 
+    m.Replace("mul(", "")
+    .Replace(")", "")
+    .Split(",")
+    .Select(x => int.Parse(x))
+    .Aggregate((a, x) => a * x)).Sum();
+
+Console.WriteLine($"Part 2 Result: {res}");
