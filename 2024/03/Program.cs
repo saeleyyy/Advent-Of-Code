b33abfabbcc1ea236121a1f7a40f59fbf9eb2953
Samuel Eley <samueleley@samuels-air.home>
2024-12-03 10:21:46 +0000

    code gold

--- 2024/03/Program.cs
+++ 2024/03/Program.cs
@@ -1,14 +1,12 @@
 ﻿using System.Text.RegularExpressions;
 
-string corruptedMemory = File.ReadAllText("input.txt");
-
-// Step 1: Get all valid blocks of mul calculations
-string regexPattern = @"mul\(\d{1,3},\d{1,3}\)";
-
-var matches = Regex.Matches(corruptedMemory, regexPattern).Select(m => m.Value);
-
-// Step 2: Parse out expressions from each match
-var total  = matches
-    .Select(m => m.Replace("mul(", "").Replace(")", "").Split(",").Select(x => int.Parse(x)).Aggregate((a, x) => a * x)).Sum();
+var total  = Regex.Matches(File.ReadAllText("input.txt"), @"mul\(\d{1,3},\d{1,3}\)")
+                .Select(m => m.Value)
+                .Select(m => 
+                    m.Replace("mul(", "")
+                        .Replace(")", "")
+                        .Split(",")
+                        .Select(x => int.Parse(x))
+                        .Aggregate((a, x) => a * x)).Sum();
 
 Console.WriteLine($"Result: {total}");
